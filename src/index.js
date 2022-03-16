c010dfef1f1b2d98182ba5945bd08d347c4f2a45
Nguyen The Ngoc <nguyenthengocit37@gmail.com>
2022-03-16 17:14:40 +0700

    Commit App Example

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const morgan = require("morgan");
+const path = require("path");
+const port = 3000;
+
+//HTTP Logger
+app.use(morgan("combined"));
+
+//Templates Engine
+
+app.engine('hbs', handlebars.engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
