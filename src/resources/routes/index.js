4ed2a324f92b6ca31e13dad95ecc738ec4c7de7f
Ngoc It <nguyenthengocit37@gmail.com>
2022-03-21 12:08:03 +0700

    Fix Bug Duplicate

--- src/resources/routes/index.js
+++
@@ -1,8 +0,0 @@
-const newRouter = require('./shop');
-const siteRouter = require('./site');
-
-function routes(app) {
-    app.use('/shop', newRouter);
-    app.use('/', siteRouter);
-}
-module.exports = routes;
--- src/resources/routes/shop.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const shopController = require('../app/controllers/ShopController');
-
-router.use('/:slug', shopController.show);
-router.use('/', shopController.index);
-
-module.exports = router;
